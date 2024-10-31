8eb0e6c7bf17b96fd3582f248554cce4c5b90f47
OKPP10 <aapupz4@gmail.com>
2024-10-31 11:22:25 +0530

    edits

--- script.js
+++
@@ -1,9 +0,0 @@
-const video = document.querySelector('.hover-video');
-
-video.addEventListener('mouseover', () => {
-  video.play();
-});
-
-video.addEventListener('mouseout', () => {
-  video.pause();
-});
